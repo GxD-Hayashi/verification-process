6f11d0a70c09e2d63da451ccefce1dc92d24279b
mkaba-gxd <mio.kabata@gxd.co.jp>
2025-11-19 14:15:52 +0900

    File path specification is now enabled.

--- post_process.py
+++ post_process.py
@@ -1,18 +1,25 @@
 import os
 import sys
+import argparse
 import glob
 import pandas as pd
 import numpy as np
 from pathlib import Path
 from openpyxl import load_workbook
 
-workdir = os.path.dirname(__file__)
-ewes_sample = [  f for f in glob.glob(os.path.join(workdir, 'CD_[0-9][0-9]_[0-9][0-9][0-9][0-9][0-9]_*[0-9]')) if os.path.isdir(f) ]
-wts_sample = [  f for f in glob.glob(os.path.join(workdir, 'CR_[0-9][0-9]_[0-9][0-9][0-9][0-9][0-9]_*[0-9]')) if os.path.isdir(f) ]
+VERSION="v1.0.0"
 
-out_file = os.path.join(workdir, 'summarized.xlsx')
-target_merge, cnv_merge, msi_merge, tmb_merge = None, None, None, None
-fusion_merge, splice_merge = None, None
+def main():
+
+    if '--help' in sys.argv or '-h' in sys.argv:
+        print(f"version: {VERSION}")
+
+    parser = argparse.ArgumentParser(description="A tool for compiling detection results of verification samples.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--directory","-d", required=False, help="Directory path where analysis data is stored.", default=None)
+    parser.add_argument("--outfile","-o", required=False, help="Output file path", default=None)
+    parser.add_argument('--version','-v', action='version', version=f'%(prog)s {VERSION}')
+    args = parser.parse_args()
+    run_summary(args)
 
 def merge_stat(merge_data, data):
     if merge_data is None :
@@ -30,82 +37,112 @@ def output_sheet(out_file, data, sheet_name):
             with pd.ExcelWriter(out_file, engine="openpyxl") as writer :
                 data.to_excel(writer, sheet_name=sheet_name, index=False)
 
-for sample in ewes_sample :
+def init(msg="") :
+    print(msg)
+    sys.exit(1)
 
-    sample_id = os.path.basename(sample)
-    cnv_file = os.path.join( sample, 'Summary', sample_id + '.summarized.cnv.exome.tsv')
-    msi_file = os.path.join( sample, 'Summary', sample_id + '.summarized.msi.exome.tsv')
-    tmb_file = os.path.join( sample, 'Summary', sample_id + '.summarized.tmb.exome.tsv')
-    target_file = os.path.join(sample, 'Summary', sample_id + '.summarized.snv.target.tsv')
+def run_summary(args):
 
-    f_flag = False
-    for f_path in [cnv_file, msi_file, tmb_file, target_file] :
-        if not os.path.isfile(f_path) : f_flag = True
-
-    if f_flag :
-        print('Summary file not created: ' + sample_id)
-        continue
-
-    cnv_data = pd.read_csv(cnv_file,sep="\t")
-    msi_data = pd.read_csv(msi_file,sep="\t")
-    tmb_data = pd.read_csv(tmb_file,sep="\t")
-    target_data = pd.read_csv(target_file,sep="\t", low_memory=False, dtype=str)
-
-    msi_data = msi_data[['MSI','Result']].drop_duplicates()
-    tmb_data = tmb_data[['TMB','TMB_STATUS']].drop_duplicates()
-    cnv_data = cnv_data[ cnv_data['FILTER']=='PASS' ][['Gene_name','TYPE','ONCOKB_ONCOGENICITY','gene.mean.CN']].drop_duplicates().sort_values('Gene_name')
-
-    msi_data.insert(0, 'sample_id', sample_id)
-    tmb_data.insert(0, 'sample_id', sample_id)
-    if cnv_data.shape[0] > 0 :
-        cnv_data.insert(0, 'sample_id', sample_id)
+    if args.directory is None :
+        workdir = os.path.dirname(__file__)
     else :
-        cnv_data = None
+        workdir = Path(args.directory)
+
+    if args.outfile is None :
+        out_file = os.path.join(workdir, 'summarized.xlsx')
+    else :
+        out_file = Path(args.outfile)
+
+    ewes_sample = [  f for f in glob.glob(os.path.join(workdir, 'CD_[0-9][0-9]_[0-9][0-9][0-9][0-9][0-9]_*[0-9]')) if os.path.isdir(f) ]
+    wts_sample = [  f for f in glob.glob(os.path.join(workdir, 'CR_[0-9][0-9]_[0-9][0-9][0-9][0-9][0-9]_*[0-9]')) if os.path.isdir(f) ]
+
+    if len(ewes_sample) == 0 and len(wts_sample) == 0 :
+        init("No inspection result data exists.")
+
+    target_merge, cnv_merge, msi_merge, tmb_merge = None, None, None, None
+    fusion_merge, splice_merge = None, None
+
+    for sample in ewes_sample :
+
+        sample_id = os.path.basename(sample)
+        cnv_file = os.path.join( sample, 'Summary', sample_id + '.summarized.cnv.exome.tsv')
+        msi_file = os.path.join( sample, 'Summary', sample_id + '.summarized.msi.exome.tsv')
+        tmb_file = os.path.join( sample, 'Summary', sample_id + '.summarized.tmb.exome.tsv')
+        target_file = os.path.join(sample, 'Summary', sample_id + '.summarized.snv.target.tsv')
+
+        f_flag = False
+        for f_path in [cnv_file, msi_file, tmb_file, target_file] :
+            if not os.path.isfile(f_path) : f_flag = True
+
+        if f_flag :
+            print('Summary file not created: ' + sample_id)
+            continue
+
+        cnv_data = pd.read_csv(cnv_file,sep="\t")
+        msi_data = pd.read_csv(msi_file,sep="\t")
+        tmb_data = pd.read_csv(tmb_file,sep="\t")
+        target_data = pd.read_csv(target_file,sep="\t", low_memory=False, dtype=str)
+
+        msi_data = msi_data[['MSI','Result']].drop_duplicates()
+        tmb_data = tmb_data[['TMB','TMB_STATUS']].drop_duplicates()
+        cnv_data = cnv_data[ cnv_data['FILTER']=='PASS' ][['Gene_name','TYPE','ONCOKB_ONCOGENICITY','gene.mean.CN']].drop_duplicates().sort_values('Gene_name')
+
+        msi_data.insert(0, 'sample_id', sample_id)
+        tmb_data.insert(0, 'sample_id', sample_id)
+
+        if cnv_data.shape[0] > 0 :
+            cnv_data.insert(0, 'sample_id', sample_id)
+        else :
+            cnv_data = None
     
-    target_data = target_data.infer_objects(copy=False).fillna(np.nan).replace([np.nan], [None])
-    target_data = target_data[['SYMBOL','HGVSc','HGVSp','AF','Clinvar_CLNSIG','ONCOKB_ONCOGENICITY']].drop_duplicates()
-    target_data["HGVSc"] = target_data["HGVSc"].str.split(":", expand=True)[1]
-    target_data["HGVSp"] = target_data["HGVSp"].str.split(":", expand=True)[1]
-    target_data.insert(0, 'sample_id', sample_id)
-    target_data['repo'] = ''
-    target_filt = (target_data["Clinvar_CLNSIG"].str.contains("Pathogenic|Likely_pathogenic", case=True, na=False) | target_data["ONCOKB_ONCOGENICITY"].str.contains("oncogenic", case=False, na=False))
-    target_data.loc[target_filt, 'Report'] = 'PASS'
+        target_data = target_data.infer_objects(copy=False).fillna(np.nan).replace([np.nan], [None])
+        target_data = target_data[['SYMBOL','HGVSc','HGVSp','AF','Clinvar_CLNSIG','ONCOKB_ONCOGENICITY']].drop_duplicates()
+        target_data["HGVSc"] = target_data["HGVSc"].str.split(":", expand=True)[1]
+        target_data["HGVSp"] = target_data["HGVSp"].str.split(":", expand=True)[1]
+        target_data.insert(0, 'sample_id', sample_id)
+        target_data['repo'] = ''
+        target_filt = (target_data["Clinvar_CLNSIG"].str.contains("Pathogenic|Likely_pathogenic", case=True, na=False) | target_data["ONCOKB_ONCOGENICITY"].str.contains("oncogenic", case=False, na=False))
+        target_data.loc[target_filt, 'Report'] = 'PASS'
 
-    cnv_merge = merge_stat(cnv_merge, cnv_data)
-    msi_merge = merge_stat(msi_merge, msi_data)
-    tmb_merge = merge_stat(tmb_merge, tmb_data)
-    target_merge = merge_stat(target_merge, target_data)
+        cnv_merge = merge_stat(cnv_merge, cnv_data)
+        msi_merge = merge_stat(msi_merge, msi_data)
+        tmb_merge = merge_stat(tmb_merge, tmb_data)
+        target_merge = merge_stat(target_merge, target_data)
 
-output_sheet(out_file, target_merge, "eWES.snv.target")
-output_sheet(out_file, cnv_merge, "eWES.cnv")
-output_sheet(out_file, msi_merge, "eWES.msi")
-output_sheet(out_file, tmb_merge, "eWES.tmb")
+    output_sheet(out_file, target_merge, "eWES.snv.target")
+    output_sheet(out_file, cnv_merge, "eWES.cnv")
+    output_sheet(out_file, msi_merge, "eWES.msi")
+    output_sheet(out_file, tmb_merge, "eWES.tmb")
     
-for sample in wts_sample :
+    for sample in wts_sample :
 
-    sample_id = os.path.basename(sample)
-    fusion_file = os.path.join(sample, 'Summary', sample_id + '.summarized.fusion.tsv')
-    splice_file = os.path.join(sample, 'Summary', sample_id + '.summarized.splice.tsv')
+        sample_id = os.path.basename(sample)
+        fusion_file = os.path.join(sample, 'Summary', sample_id + '.summarized.fusion.tsv')
+        splice_file = os.path.join(sample, 'Summary', sample_id + '.summarized.splice.tsv')
 
-    if os.path.isfile(fusion_file) :
-        fs_data = pd.read_csv(fusion_file, sep="\t")
-        fs_data = fs_data[['gene1','gene2','chr1','breakpoint_1','chr2','breakpoint_2','max_split_cnt','max_span_cnt']].drop_duplicates().sort_values('gene1').reset_index(drop=True)
-        fs_data.insert(0, 'sample_id', sample_id)
-    else :
-        print('fusion summary file not created: ' + sample_id)
-        fs_data = None
+        if os.path.isfile(fusion_file) :
+            fs_data = pd.read_csv(fusion_file, sep="\t")
+            fs_data = fs_data[['gene1','gene2','chr1','breakpoint_1','chr2','breakpoint_2','max_split_cnt','max_span_cnt']].drop_duplicates().sort_values('gene1').reset_index(drop=True)
+            fs_data.insert(0, 'sample_id', sample_id)
+        else :
+            print('fusion summary file not created: ' + sample_id)
+            fs_data = None
 
-    if os.path.isfile(splice_file) :
-        sp_data = pd.read_csv(splice_file, sep="\t")
-        sp_data = sp_data[ sp_data['FILTER'] == 'PASS' ][['spliceName','discordant_mates','canonical_reads','ratio','tpm_total','tpm_variant']].drop_duplicates()
-        sp_data.insert(0, 'sample_id', sample_id)
-    else :
-        print('splice summary file not created: ' + sample_id)
-        sp_data = None
+        if os.path.isfile(splice_file) :
+            sp_data = pd.read_csv(splice_file, sep="\t")
+            sp_data = sp_data[ sp_data['FILTER'] == 'PASS' ][['spliceName','discordant_mates','canonical_reads','ratio','tpm_total','tpm_variant']].drop_duplicates()
+            sp_data.insert(0, 'sample_id', sample_id)
+        else :
+            print('splice summary file not created: ' + sample_id)
+            sp_data = None
 
-    fusion_merge = merge_stat(fusion_merge, fs_data)
-    splice_merge = merge_stat(splice_merge, sp_data)
+        fusion_merge = merge_stat(fusion_merge, fs_data)
+        splice_merge = merge_stat(splice_merge, sp_data)
 
-output_sheet(out_file, fusion_merge, "WTS.fusion")
-output_sheet(out_file, splice_merge, "WTS.splice")
+    output_sheet(out_file, fusion_merge, "WTS.fusion")
+    output_sheet(out_file, splice_merge, "WTS.splice")
+
+if __name__ == "__main__":
+
+    main()
 
